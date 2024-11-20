fe6671cfff680c14fddd613c6aecbcb56ba2c31f
Apple Sun <1160948478@qq.com>
2024-11-20 23:53:23 +0800

    modify .env file

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,10 @@
 import dotenv from 'dotenv';
-import path from 'path';
+import { resolve } from 'path';
+import { existsSync } from 'fs';
 import Joi from 'joi';
 
-const __dirname = path.resolve();
-dotenv.config({ path: path.resolve(__dirname, `.env.${process.env.NODE_ENV}` || '.env') });
+const envFilePath = resolve(process.cwd(), `.env.${process.env.NODE_ENV}`);
+dotenv.config({ path: existsSync(envFilePath) ? envFilePath : resolve(process.cwd(), '.env') });
 
 const envVarsSchema = Joi.object()
   .keys({
